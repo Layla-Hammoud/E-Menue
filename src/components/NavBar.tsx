e9393cf45d8b030a959589c959345ac716668673
Layla Hammoud <laylahammoud63@gmail.com>
2023-05-23 21:51:26 +0300

    responsive navbar
    work on navbar to make it responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,25 @@
+import * as React from 'react';
 import './NavBar.css'
 import GridIcon from './grid_icon.svg'
+import IconMenu from './icon-menu.svg'
+import XSymbol from './x-symbol.svg'
 function NavBar() {
+    const [isMobile,setIsMobile]=React.useState<Boolean>(false);
     return (
         <>
             <nav>
                 <div className='brand'>
-                <h1>Food Grid</h1>
+                <a href="#"><h1>Food Grid</h1></a>
                 <img src={GridIcon}></img>
                 </div>
-                <ul>
-                    <li>Home</li>
-                    <li>Become a vendor</li>
-                    <li>Menus</li>
-                    <li>About us</li>
+                <ul className={isMobile? "nav-links-mobile" : "nav-links"} onClick={()=>setIsMobile(false)}>
+                <a href="#" className="Nav-item mobile-item"><li>Home</li></a>
+                <a href="#" className="Nav-item mobile-item"><li>Become a vendor</li></a>
+                <a href="#"className="Nav-item mobile-item"><li>Menus</li></a>
+                <a href="#" className="Nav-item mobile-item"><li>About us</li></a>
+                <a href="#"><div className={isMobile? ".signup-button-mobile" : "signup-button"}>Sign up</div></a>
                 </ul>
-                <div className='signup-button'>Sign up</div>
+                <img className='mobile-menu-icon' src={isMobile? XSymbol : IconMenu} onClick={()=>setIsMobile(!isMobile)}></img>
             </nav>
         </>
     )
